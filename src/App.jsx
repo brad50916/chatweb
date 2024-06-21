d13a777bf75140268b89cb5a9c21b33bf814359f
Bard <brad50916@gmail.com>
2024-06-21 21:53:28 +0800

    add signup and signin component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import Button from '@mui/material/Button';
 import Dashboard from './component/Dashboard';
+import Signup from './component/Signup';
+import Signin from './component/Signin';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <Dashboard />
+    <BrowserRouter>
+      <Routes>
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="signin" element={<Signin />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
